364cf64ce4a13badb210db444f9969e8dcc809eb
jonah-kayiwa <kayiwa.jonah@student.moringaschool.com>
2023-12-29 14:59:16 +0300

    ##function continued##

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // code your solution here
+
+// to generate fun activities fir saturday //
+function saturdayFun(activity = 'roller-skate') {
+    return `This Saturday, let's ${activity}!`;
+  }
+  
+  // to generate monday work //
+  function mondayWork(task = 'go to the office') {
+    return `This Monday, I need to ${task}.`;
+  }
+  
+  //  to wrap an adjectiv with a visual flair //
+  function wrapAdjective(visualFlair = '*') {
+    return function(adjective = 'special') {
+      return `You are ${visualFlair}${adjective}${visualFlair}`;
+    };
+  }
+  
+  // Example usage of wrapAdjective //
+  const encouragingPromptFunction = wrapAdjective('!!!');
+  
+  
